3faf8d1521667461b0aaca9630b86eb869c90bb4
whatrocks <charles.harrington@gmail.com>
2015-10-19 15:48:37 -0700

    Impliment fetch passing test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,43 @@
 // YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init: function(){
+
+  },
+  send: function(message){
+    $.ajax({
+      url: app.server,
+      type: "POST",
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data){
+        console.log("chatterbox: message sent");
+      },
+      error: function(data){
+        console.error("chatterbox: failed to send message");
+      }
+    });
+  },
+  fetch: function(){
+
+    $.ajax({
+      url: app.server,
+      type: "GET",
+      dataType: "json",
+      contentType: 'application/json',
+      success: function(data) {
+        _.each(data.results, function(element, index){
+           $('#chats').append('<div class="message">' + element.username + ': ' + element.text + '</div>');
+        });
+      },
+      error: function(data) {
+        console.error("errorroror");
+      }
+    });
+  },
+  clearMessages: function(){
+
+  }
+};
+
+app.fetch();
